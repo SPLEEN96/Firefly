a760be78a15931f6ac0f5d23841794d9ebbbc364
SPLEEN96 <spleen96@scorp.org>
2019-01-18 18:49:47 +0000

    Added basic implementation of spdlog

--- Firefly/Src/Firefly/Logger.cpp
+++ Firefly/Src/Firefly/Logger.cpp
@@ -2,7 +2,20 @@
 
 namespace Firefly {
 
-Logger::Logger(){}
-Logger::~Logger(){}
+std::shared_ptr<spdlog::logger> Logger::m_core_logger;
+std::shared_ptr<spdlog::logger> Logger::m_client_logger;
+
+Logger::Logger() {}
+Logger::~Logger() {}
+
+void Logger::Init() {
+    spdlog::set_pattern("%^[%T] %n: %v%$"); /* Timestamp Name : Message */
+
+    m_core_logger = spdlog::stdout_color_mt("FIREFLY");
+    m_core_logger->set_level(spdlog::level::level_enum::trace);
+
+    m_client_logger = spdlog::stdout_color_mt("APPLICATION");
+    m_client_logger->set_level(spdlog::level::level_enum::trace);
+}
 
 }  // namespace Firefly
