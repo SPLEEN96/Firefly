c7609323530f594f88c1b29d9caf117d7b2a592a
SPLEEN96 <spleen96@scorp.org>
2019-01-30 15:04:34 +0000

    Added typedef for int and uint sizes

--- Firefly/Src/PCH_CORE.h
+++ Firefly/Src/PCH_CORE.h
@@ -7,3 +7,11 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+
+typedef uint64_t ui64;
+typedef uint32_t ui32;
+typedef uint16_t ui16;
