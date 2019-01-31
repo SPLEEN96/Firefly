d7a18860123bbe0eb485bea5fe98ece6fff7e2ef
SPLEEN96 <guay.nicolas1996@gmail.com>
2019-01-31 14:45:11 +0000

    Removed typedefs from PCH_CORE (now in FFLY_CORE)

--- Firefly/Src/PCH_CORE.h
+++ Firefly/Src/PCH_CORE.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Firefly/Logger.h"
+
 #include <iostream>
 #include <sstream>
 #include <memory>
@@ -7,11 +9,3 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-
-typedef int64_t i64;
-typedef int32_t i32;
-typedef int16_t i16;
-
-typedef uint64_t ui64;
-typedef uint32_t ui32;
-typedef uint16_t ui16;
