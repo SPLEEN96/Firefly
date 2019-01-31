9b18f7285874b19ac441c284cb528b2c450d0dbe
SPLEEN96 <spleen96@scorp.org>
2019-01-31 19:23:31 +0000

    Basic Event system implementation

--- Firefly/Src/Firefly/Events/KeyEvents.h
+++ Firefly/Src/Firefly/Events/KeyEvents.h
@@ -5,21 +5,19 @@ namespace Firefly {
 
 class KeyEvent : public Event {
   public:
-    EVENT_CLASS_CATEGORY(ECATEGORY_INPUT | ECATEGORY_KEYBOARD);
-
     const ui16& GetKeyCode() const { return m_keycode; }
 
   protected:
-    KeyEvent(ui16 keycode) : m_keycode(keycode) {}
+    KeyEvent(EventType type, ui16 keycode)
+        : Event(type, (EventCategory)(ECATEGORY_INPUT | ECATEGORY_KEYBOARD)),
+          m_keycode(keycode) {}
     ui16 m_keycode;
 };
 
 class KeyPressedEvent : public KeyEvent {
   public:
     KeyPressedEvent(ui16 keycode, i16 repeat_count)
-        : KeyEvent(keycode), m_repeat_count(repeat_count) {}
-
-    EVENT_CLASS_TYPE(E_KEY_PRESSED);
+        : KeyEvent(E_KEY_PRESSED, keycode), m_repeat_count(repeat_count) {}
 
     std::string ToString() const override {
         std::stringstream sstream;
@@ -36,13 +34,11 @@ class KeyPressedEvent : public KeyEvent {
 
 class KeyReleasedEvent : public KeyEvent {
   public:
-    KeyReleasedEvent(ui16 keycode) : KeyEvent(keycode) {}
-
-    EVENT_CLASS_TYPE(E_KEY_PRESSED);
+    KeyReleasedEvent(ui16 keycode) : KeyEvent(E_KEY_RELEASED, keycode) {}
 
     std::string ToString() const override {
         std::stringstream sstream;
-        sstream << "KeyPressedEvent:" << m_keycode;
+        sstream << "KeyReleasedEvent:" << m_keycode;
         return sstream.str();
     }
 };
