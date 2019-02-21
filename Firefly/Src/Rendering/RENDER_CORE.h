b6ee855a30085841107db7536d2f452a1ea1bb67
SPLEEN96 <guay.nicolas1996@gmail.com>
2019-02-21 14:10:34 +0000

    VK_ASSERT now end the execution of the program

--- Firefly/Src/Rendering/RENDER_CORE.h
+++ Firefly/Src/Rendering/RENDER_CORE.h
@@ -9,7 +9,8 @@ namespace Rendering {
 #define VK_ASSERT(x, ...)                                                               \
     {                                                                                   \
         if (x != VK_SUCCESS) {                                                          \
-            FFLY_LOG_CORE_CRITICAL(__VA_ARGS__);                                        \
+            FFLY_LOG_CORE_CRITICAL("VULKAN::{0}", __VA_ARGS__);                         \
+            *(int*)0 = 0;                                                               \
         }                                                                               \
     }
 
