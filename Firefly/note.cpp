152b9498568a089855ee8b482f22048801d6228c
SPLEEN96 <guay.nicolas1996@gmail.com>
2019-11-30 16:31:51 +0000

    notes

---
+++ Firefly/note.cpp
@@ -0,0 +1,20 @@
+// TODO:
+//
+// -MemoryManager
+//		-StackBasedAllocator
+//		-MemoryAllignment
+//		-MemoryProfiler
+
+// -FileSystemManager
+// -RessourceFactory
+// -Root
+
+// -2D collision : https://www.youtube.com/watch?v=7Ik2vowGcU0
+
+#include "FFLY_CORE.h"
+#include "Firefly/Events/KeyEvents.h"
+#include "PCH_CORE.h"
+
+enum class keycodes : uint8 {
+
+};
