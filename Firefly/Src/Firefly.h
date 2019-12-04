9fa39dfe77421ec89ee6d53ee9a7cb74c451cc85
SPLEEN96 <guay.nicolas1996@gmail.com>
2019-12-04 16:28:05 +0000

    Header to include in Project using Firefly

---
+++ Firefly/Src/Firefly.h
@@ -0,0 +1,13 @@
+#include "PCH_CORE.h"
+#include "FFLY_CORE.h"
+
+/* === CORE === */
+#include "Core/Application.h"
+#include "Core/Layer.h"
+
+#include "Imgui/ImguiLayer.h"
+
+/* === Rendering === */
+#include "Rendering/RenderModule.h"
+
+#include "Rendering/Buffer.h"
