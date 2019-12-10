c0339ac3df5bc3c8d490d9f5f24456d32140cd44
SPLEEN96 <guay.nicolas1996@gmail.com>
2019-12-10 22:49:48 +0000

    Testing the new Texture implementation

--- Firefly/Src/Sandbox/Sandbox.cpp
+++ Firefly/Src/Sandbox/Sandbox.cpp
@@ -7,8 +7,6 @@
 #include <imgui/examples/imgui_impl_opengl3.h>
 
 #include <glad/glad.h>
-#include <Rendering/Buffer.h>
-#include <Rendering/Shader.h>
 
 #define FFLY Firefly::Rendering
 
@@ -23,24 +21,43 @@ class TriangleLayer : public Firefly::Layer {
 
         std::vector<FFLY::VertexAttribute> vattr = {
             FFLY::VertexAttribute("position", FFLY::VertexAttribute::AttrType::FLOAT3),
-            FFLY::VertexAttribute("color", FFLY::VertexAttribute::AttrType::FLOAT3)};
+            FFLY::VertexAttribute("color", FFLY::VertexAttribute::AttrType::FLOAT3),
+            FFLY::VertexAttribute("tex_coord", FFLY::VertexAttribute::AttrType::FLOAT2)};
 
         FFLY::VertexBuffer* vbuffer =
-            new FFLY::VertexBuffer(m_vertices, sizeof(m_vertices), vattr);
+            new FFLY::VertexBuffer(m_quad_vertices, sizeof(m_quad_vertices), vattr);
         vbuffer->Bind();
 
         m_triangle_shader =
             Firefly::Factory::Shader::Create("./Assets/Shaders/TriangleTest");
+
+        m_square_shader =
+            Firefly::Factory::Shader::Create("./Assets/Shaders/TexturedQuad");
+
+        m_texture =
+            Firefly::Factory::Texture::Create("./Assets/Textures/obsidian.png", false);
+
+        m_texture2 =
+            Firefly::Factory::Texture::Create("./Assets/Textures/grey_stone.jpg", false);
     }
 
     virtual void OnDetach() override {}
 
     virtual void OnUpdate() override {
-        m_triangle_shader->Bind();
-        m_triangle_shader->SetUniform3fv("u_color", m_color.x, m_color.y, m_color.z);
+        m_square_shader->Bind();
+        m_square_shader->SetUniform1i("tex", 0);
+        m_texture->Bind(0);
+        //m_square_shader->SetUniform1i("tex", 1);
+        //m_texture2->Bind(1);
 
         glBindVertexArray(m_VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        m_triangle_shader->Bind();
+        m_triangle_shader->SetUniform3fv("u_color", m_color.x, m_color.y, m_color.z);
+        glBindVertexArray(m_VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glBindVertexArray(0);
     }
 
@@ -71,12 +88,79 @@ class TriangleLayer : public Firefly::Layer {
     virtual void OnEvent(Firefly::Event& event) override {}
 
   private:
-    uint32        m_VAO;
-    FFLY::Shader* m_triangle_shader;
-    ImVec4        m_color = ImVec4(0.4f, 0.f, 0.f, 1.f);
+    uint32         m_VAO;
+    FFLY::Shader * m_triangle_shader, *m_square_shader;
+    FFLY::Texture *m_texture, *m_texture2;
+    ImVec4         m_color = ImVec4(0.4f, 0.f, 0.f, 1.f);
 
-    float m_vertices[6 * 3] = {-0.5f, -0.5f, 0.0f, 1.f,  0.f,  0.f,  0.5f, -0.5f, 0.0f,
-                               0.f,   1.f,   0.f,  0.0f, 0.5f, 0.0f, 0.f,  0.f,   1.f};
+    float m_quad_vertices[8 * 6] = {
+        /* Positions */ /* Colors */ /* TexCoords */
+        0.5f,
+        0.5f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        1.0f, /* top right */
+        0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        1.0f,
+        0.0f, /* bottom right */
+        -0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f, /* bottom left */
+        0.5f,
+        0.5f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        1.0f, /* top right */
+        -0.5f,
+        0.5f,
+        0.0f,
+        1.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        1.0f, /* top left */
+        -0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f, /* bottom left */
+    };
+
+    float m_triangle_vertices[6 * 3] = {/* Positions */ /* Colors */
+                                        -0.5f,
+                                        -0.5f,
+                                        0.0f,
+                                        1.f,
+                                        0.f, /* left vertex */
+                                        0.5f,
+                                        -0.5f,
+                                        0.0f,
+                                        0.f,
+                                        1.f, /* right vertex */
+                                        0.0f,
+                                        0.5f,
+                                        0.0f,
+                                        0.f,
+                                        0.f}; /* top vertex */
 };
 /* =================================================================*/
 
